aba634d64aab1f2ba0c995a90783a05e6b28b732
fernandocfbf <fernandocfbf@al.insper.edu.br>
2021-03-18 15:36:17 -0300

    532 testes passando

--- geometry.c
+++ geometry.c
@@ -1,5 +1,31 @@
 #include <math.h>
 #include "geometry.h"
+#include <stdio.h>
+
+int min(int x, int y){
+  return (x < y) ? x : y;
+}
+
+int max(int x, int y){
+  return (x > y) ? x : y;
+}
+
+point retorna_menor_ponto(point a, point b){
+
+    if (a.x < b.x){
+        return a;
+    }
+    return b;
+}
+
+point retorna_maior_ponto(point a, point b){
+
+    if(a.x < b.x){
+        return b;
+    }
+
+    return a;
+}
 
 double eq_reta(int x1, int y1, double m, int x_para_calcular){
 
@@ -8,7 +34,6 @@ double eq_reta(int x1, int y1, double m, int x_para_calcular){
 }
 
 int verify(point p, point a, point b) {
-
     
     double m = 0; //coneficiente angular da reta partindo do ponto p
     double m_aresta = (double) (b.y - a.y)/(b.x - a.x); //coneficiente angular do segmento de reta
@@ -17,20 +42,89 @@ int verify(point p, point a, point b) {
     //este seja igual o p.y e m = m_aresta, significa que a eq da reta é a mesma
     double y_calculado_aresta = eq_reta(a.x, a.y, m_aresta, p.x);
 
-    double paralelismo = fabs(m - m_aresta);
-    double igualidade = fabs(y_calculado_aresta - p.y);
+    double paralelismo = fabs(m - m_aresta);  // se m do robô é igual ao m da aresta
+    double igualidade = fabs(y_calculado_aresta - p.y); // se ponto p está na aresta 
 
-    //se o coeficiente for o mesmo, mas os pontos não coincidirem,
-    //significa que as retas são paralelas
-    if(paralelismo < 0.000001 && igualidade >= 0.000001){
+    point menor_ponto = retorna_menor_ponto(a, b);
+    point maior_ponto = retorna_maior_ponto(a, b);
+
+    //1 - > nao é igual
+
+    //printf("verifica: %d\n", igualidade >= 0.000001);
+
+    // ------------------------------------- CASOS ESPECÍFICOS ------------------------------------- 
+
+    if(igualidade < 0.000001){
+        return 2;
+    }
+
+    //se o coeficiente for o mesmo, mas os pontos não coincidirem, significa que as retas não tem intersecção (check)
+    else if(paralelismo < 0.000001 && p.x > maior_ponto.x){
         return 0;
     } 
 
-    else if(paralelismo < 0.000001 && igualidade < 0.000001){
-        return 2;
+    //se as retas são paralelas e p está na mesma direção da aresta mas não sobre ela, existe intersecção (check)
+    else if(paralelismo < 0.000001 && p.x < menor_ponto.x && p.y == menor_ponto.y){
+        return 1;
     }
-    
-    return 1;
+
+    // ---------------------------------------------------------------------------------------------
+
+    // ------------------------------------- CASOS GERAIS -------------------------------------
+
+    else{
+
+        //verificar se as retas se cruzam
+
+        //minimo valor de x para a aresta
+        int aresta_x_min = min(a.x, b.x);
+
+        //maximo valor de x para a aresta
+        int aresta_x_max = max(a.x, b.x);
+
+        //minimo valor de x para raio do robô
+        int robo_x_min = p.x;
+
+        //maximo valor de x para raio do robô
+        int robo_x_max = 2147483647;
+
+        if(aresta_x_min > robo_x_min && robo_x_max < aresta_x_min){
+            return 0;
+        }
+
+        if(robo_x_min > aresta_x_min && aresta_x_max < robo_x_min){
+            return 0;
+        }
+
+        //y = A*x + b
+
+        //equacao da reta robo 
+        double b1 = p.y;
+
+        //equacao da reta para a aresta 
+        double b2 = a.y - m_aresta*a.x;
+
+        //para intersectar o X deve ser igual nas duas equacoes
+        double xa = (b2-b1)/(-m_aresta);
+
+        //para possuir intersecção (ponto A), precisamos que o x de A
+        //esteja contido no intervalo entre do menor x e o maior x para
+        //as duas retas
+
+        int ax_min = max(aresta_x_min, robo_x_min);
+        int ax_max = min(aresta_x_max, robo_x_max);
+        
+        if(xa > ax_max || xa < ax_min){
+            return 0;
+        }
+
+        else{
+            return 1;
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------
+    return 0;
 }
 
 int inside(point p, point poly[], int n) {
